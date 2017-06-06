e8d7fbb5d87e506b130c075de5e44c06a57e56d3
larryg727 <larrygonzales727@gmail.com>
2017-06-06 10:18:10 -0500

    -completed servlet exercises and bonus

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by larryg on 6/6/17.
+ */
+
+@WebServlet(name="CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet{
+    int counter = 0;
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    counter += 1;
+    String reset = request.getParameter("reset");
+
+    if("true".equalsIgnoreCase(reset)){
+        counter = 0;
+        out.println("<h1>Page views: " + counter + "</h1>");
+
+    }else {
+        out.println("<h1>Page views: " + counter + "</h1>");
+    }
+}
+}
