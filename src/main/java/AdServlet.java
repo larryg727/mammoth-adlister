e4de8304412625ca8b333a624616d0ee27999963
larryg727 <larrygonzales727@gmail.com>
2017-06-07 17:11:47 -0500

    -completed mvc exercise

---
+++ src/main/java/AdServlet.java
@@ -0,0 +1,25 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by larryg on 6/7/17.
+ */
+@WebServlet(name = "AdServlet", urlPatterns = "/ads")
+public class AdServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Ad> adList;
+        adList = DaoFactory.getAdsDao().all();
+
+        request.setAttribute("ads", adList);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
+
+    }
+}
---
+++ src/main/webapp/ads/index.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <jsp:include page="../partials/head.jsp">
+        <jsp:param name="title" value="Welcome to my site!" />
+    </jsp:include>
+</head>
+<body>
+<jsp:include page="../partials/navbar.jsp" />
+<div class="container">
+    <h1>Welcome to the Adlister ad page!</h1>
+
+    <c:forEach var="ad" items="${ads}">
+        <p>Title: ${ad.title}</p>
+        <p>Description: ${ad.description}</p>
+    </c:forEach>
+</div>
+</body>
+</html>
